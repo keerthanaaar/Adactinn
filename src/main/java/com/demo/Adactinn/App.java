75be265a7224578f236b9f29078604b9b808bf9b
keerthanaaar <keerthanaa436@gmail.com>
2023-03-27 22:11:12 +0530

    Project Updated

---
+++ src/main/java/com/demo/Adactinn/App.java
@@ -0,0 +1,13 @@
+package com.demo.Adactinn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
